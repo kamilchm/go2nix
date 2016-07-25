cbf10a04faa31904fdc8cf7a7f3152f2e421bd54
Kamil Chmielewski <kamil.chm@gmail.com>
2016-07-25 18:40:45 +0200

    Helper script to migrate `deps.json` to `deps.nix`

---
+++ depsjson2nix.nix
@@ -0,0 +1,38 @@
+# helps to migrate deps.json -> deps.nix
+# eg. cat $(nix-build -E "import ./depsjson2nix.nix ~/nixpkgs/pkgs/servers/caddy/deps.json") > ~/nixpkgs/pkgs/servers/caddy/deps.nix
+depsJson:
+with import <nixpkgs>{};
+with builtins;
+
+let
+  dep2nix = goDep: with goDep; ''
+    {
+      goPackagePath = "${goPackagePath}";
+      src = fetch${fetch.type} {
+        url = "${fetch.url}";
+        rev = "${fetch.rev}";
+        sha256 = "${fetch.sha256}";
+      };
+    }
+    '';
+
+  importGodeps = { depsFile, filterPackages ? [] }:
+  let
+    deps = lib.importJSON depsFile;
+    external = filter (d: d ? include) deps;
+    direct = filter (d: d ? goPackagePath && (length filterPackages == 0 || elem d.goPackagePath filterPackages)) deps;
+  in
+    foldl' (p: n: p + n) "{ fetchgit, fetchhg, fetchbzr, fetchsvn }:\n[\n"
+    ((map importGodeps (map (d: { depsFile = <nixpkgs> + "/pkgs/development/go-modules/generic/" + d.include; filterPackages = d.packages; }) external)) ++ (map dep2nix direct)) + "]\n";
+
+  depsNix = toFile "deps.nix" (importGodeps { depsFile = toPath depsJson; });
+in
+stdenv.mkDerivation {
+  name = "goDeps";
+  src = depsJson;
+  phases = [ "installPhase" ];
+
+  installPhase = ''
+    cp ${depsNix} $out
+  '';
+}
