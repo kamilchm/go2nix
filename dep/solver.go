d37c8a3eaeae6503c0356d683b5b576df8027da5
Kamil Chmielewski <kamil.chm@gmail.com>
2017-06-20 15:58:31 +0200

    don't use dep/internal/gps, read Gopkg.lock directly

--- dep/solver.go
+++ dep/solver.go
@@ -3,11 +3,26 @@ package dep
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
-	"github.com/golang/dep"
+	"github.com/pelletier/go-toml"
 
 	"github.com/kamilchm/go2nix"
 )
+
+const (
+	lockFile = "Gopkg.lock"
+)
+
+type depLock struct {
+	Projects []depProject `toml:"projects"`
+}
+
+type depProject struct {
+	Name     string   `toml:"name"`
+	Revision string   `toml:"revision"`
+	Packages []string `toml:"packages"`
+}
 
 type Solver struct{}
 
@@ -16,29 +31,37 @@ func (s *Solver) Dependencies(pkg go2nix.GoPackage, goPath string) ([]go2nix.GoP
 	if err != nil {
 		return nil, fmt.Errorf("Couldn't get working dir: %v", err)
 	}
-	ctx, err := dep.NewContext(wd, []string{"GOPATH=" + goPath}, nil)
+	lock, err := readLock(wd)
 	if err != nil {
-		return nil, fmt.Errorf("Couldn't create Go dep context: %v", err)
+		return nil, fmt.Errorf("Couldn't read Dep lock file: %v", err)
 	}
 
-	project, err := ctx.LoadProject("")
-	if err != nil {
-		return nil, fmt.Errorf("Couldn't load Go project: %v", err)
-	}
-
-	return convertDeps(project), nil
+	return convertDeps(lock), nil
 }
 
-func convertDeps(depProject *dep.Project) (deps []go2nix.GoPackage) {
-	for _, p := range depProject.Lock.Projects() {
-		pkg := go2nix.GoPackage{
-			Name:    go2nix.ImportPath(p.Ident().ProjectRoot),
-			Version: p.Version().String(),
-		}
+func readLock(dir string) (*depLock, error) {
+	lPath := filepath.Join(dir, lockFile)
+	lTree, err := toml.LoadFile(lPath)
+	if err != nil {
+		return nil, fmt.Errorf("Couldn't load %s: %v", lPath, err)
+	}
 
-		//if v, ok := p.Version().(gps.PairedVersion); ok {
-		//	pkg.Revision = v.Underlying().String()
-		//}
+	lock := &depLock{}
+	err = lTree.Unmarshal(lock)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to parse dep lock tree: %v", err)
+	}
+
+	return lock, nil
+}
+
+func convertDeps(lock *depLock) (deps []go2nix.GoPackage) {
+	for _, p := range lock.Projects {
+		pkg := go2nix.GoPackage{
+			Name:        go2nix.ImportPath(p.Name),
+			Revision:    p.Revision,
+			SubPackages: p.Packages,
+		}
 
 		deps = append(deps, pkg)
 	}
